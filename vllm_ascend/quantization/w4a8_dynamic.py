5caf1660d28896b9aa09c0efd0048514fce5601e
pichangping <1337510399@qq.com>
2025-06-25 20:26:43 +0800

    Update w4a8_dynamic.py

--- vllm_ascend/quantization/w4a8_dynamic.py
+++ vllm_ascend/quantization/w4a8_dynamic.py
@@ -19,15 +19,15 @@ from typing import Any, Callable, Dict, Optional
 
 import numpy as np
 import torch
-import torch.distributed as dist
 import torch_npu
 from vllm.config import get_current_vllm_config
 
 from vllm_ascend.ascend_config import get_ascend_config
 from vllm_ascend.distributed.parallel_state import get_ep_group
 from vllm_ascend.ops.fused_moe import select_experts
-from vllm_ascend.quantization.w8a8_dynamic import fused_experts_with_mc2, fused_experts_with_all2all
-from vllm_ascend.utils import (FusedMoEState, get_fused_moe_state)
+from vllm_ascend.quantization.w8a8_dynamic import (fused_experts_with_all2all,
+                                                   fused_experts_with_mc2)
+from vllm_ascend.utils import FusedMoEState, get_fused_moe_state
 
 
 class AscendW4A8DynamicLinearMethod:
