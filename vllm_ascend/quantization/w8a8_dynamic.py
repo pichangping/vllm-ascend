bda32694e6af83843a7dc9bc855e1bab7b72ff57
pichangping <1337510399@qq.com>
2025-12-05 11:33:51 +0800

    Update w8a8_dynamic.py

--- vllm_ascend/quantization/w8a8_dynamic.py
+++ vllm_ascend/quantization/w8a8_dynamic.py
@@ -3,7 +3,7 @@
 # This file is a part of the vllm-ascend project.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# you may not use this file except in compliance with the License.1
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
